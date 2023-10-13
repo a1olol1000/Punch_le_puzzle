f1ad50020827b06efd11493c28fc1befd4581d8a
Albin Lindqvist <albin.lindqvist@elev.ga.ntig.se>
2023-10-13 12:24:18 +0200

    added asic physics

--- Assets/scripts/characterMover.cs
+++ Assets/scripts/characterMover.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class characterMover : MonoBehaviour
 {
+    [SerializeField]
+    float jumpForze = 100f;
+    [SerializeField]
+    float walkSpeed = 7.5f;
+    [SerializeField]
+    float runSpeed = 10f;
+    float run = 0f;
     private inputManager inputManager;
+    Rigidbody riibody;
     float upp ;
+    float horisontal;
+    float vertical;
     // Start is called before the first frame update
+    void Awake() 
+    {
+        riibody = GetComponent<Rigidbody>();
+    }
     void Start()
     {
         inputManager = inputManager.instance;
@@ -17,10 +32,52 @@ public class characterMover : MonoBehaviour
     {
         if (inputManager.GetKeyDown(KeybindingActions.Jump))
         {
-            upp ++;
+            riibody.AddForce(Vector3.up * jumpForze);
         }
-        
-        Vector3 move = new Vector3(0, 0, upp) * Time.deltaTime;
+        float walk = walkSpeed;
+        if (inputManager.GetKey(KeybindingActions.Run))
+        {
+            run = runSpeed;
+        }
+        else if (!inputManager.GetKey(KeybindingActions.Run))
+        {
+            run = 0f;
+        }
+        if (inputManager.GetKey(KeybindingActions.Walkforward)&&inputManager.GetKey(KeybindingActions.Walkbakward))
+        {
+            vertical = 0;
+        }
+        else if (inputManager.GetKey(KeybindingActions.Walkforward))
+        {
+            vertical = 1;
+        }
+        else if (inputManager.GetKey(KeybindingActions.Walkbakward))
+        {
+            vertical = -1;
+        }
+        else
+        {
+            vertical = 0;
+        }
+        if (inputManager.GetKey(KeybindingActions.StafeLeft)&&inputManager.GetKey(KeybindingActions.StrafeRight))
+        {
+            horisontal = 0;
+        }
+        else if (inputManager.GetKey(KeybindingActions.StafeLeft))
+        {
+            horisontal = 1;
+        }
+        else if (inputManager.GetKey(KeybindingActions.StrafeRight))
+        {
+            horisontal = -1;
+        }
+        else
+        {
+            horisontal = 0;
+        }
+        walk = walk + run;
+        Vector3 move = new Vector3(-vertical, horisontal, upp) * walk * Time.deltaTime;
         transform.Translate(move);
+        
     }
 }
