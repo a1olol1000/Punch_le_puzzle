17005413f5f1cc63047bac7eab914f97e40f67bf
Albin Lindqvist <albin.lindqvist@elev.ga.ntig.se>
2023-10-18 14:15:19 +0200

    dum kod för att flytta kameran

--- Assets/scripts/characterMover.cs
+++ Assets/scripts/characterMover.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 public class characterMover : MonoBehaviour
 {
-    
+    [SerializeField]
+    Transform facecamera;
+    [SerializeField]
+    GameObject faceCamera;
+    int xposm=0;
+    int yposm=0;
     int framecurent=0;
+    Vector3 mosepotisionDelta;
     Vector3 rotate;
-    Vector3 mousePosition1;
+    Vector3 mouseSavePosition1;
     Vector3 mousePosition2;
     [SerializeField]
     float spinspeed = 1f;
+        [SerializeField]
+    float tiltspeed = 10f;
     [SerializeField]
     float jumpForze = 100f;
     [SerializeField]
@@ -32,17 +44,18 @@ public class characterMover : MonoBehaviour
     }
     void Start()
     {
+        mousePosition2 = Input.mousePosition;
         inputManager = inputManager.instance;
+        xposm = (int)mousePosition2.x;
+        yposm = (int)mousePosition2.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        framecurent ++;
-        if (framecurent>1)
-        {
-            framecurent = 0;
-        }
+        int xPos = xposm, yPos = yposm;
+
+        
         if (inputManager.GetKeyDown(KeybindingActions.Jump))
         {
             riibody.AddForce(Vector3.up * jumpForze);
@@ -94,19 +107,43 @@ public class characterMover : MonoBehaviour
         move = move * walk * Time.deltaTime;
         transform.Translate(move);
         transform.Rotate(0,0,rotate.z*spinspeed*Time.deltaTime);
+        faceCamera.transform.Rotate(rotate.y*tiltspeed*Time.deltaTime,0,0);
+        rotate.x = 0;
+        rotate.y=0;
+        rotate.z=0;
+        if (inputManager.GetKey(KeybindingActions.Interackt))
+        {
+            mouseSavePosition1 = Input.mousePosition;
+            mosepotisionDelta= mouseSavePosition1 - mousePosition2;
+            if (Input.GetAxis("Mouse X")>0)
+            {
+                rotate.z = -mosepotisionDelta.x;
+            }
+            else if (Input.GetAxis("Mouse X")<0)
+            {
+                rotate.z = mosepotisionDelta.x;
+            }
+            else 
+            {
+                rotate.z = 0;
+            }
+            if (Input.GetAxis("Mouse Y")>0)
+            {
+                rotate.y = mosepotisionDelta.y;
+            }
+            else if (Input.GetAxis("Mouse Y")<0)
+            {
+                rotate.y = -mosepotisionDelta.y;
+            }
+            else 
+            {
+                rotate.y = 0;
+            }
+            SetCursorPos(xPos,yPos);
+        }  
         
     }
-    private void OnMouseDrag() 
-    {
-        if (framecurent == 0)
-        {mousePosition1 = Input.mousePosition;}
-        if (framecurent == 1)
-        {mousePosition2 = Input.mousePosition;}
-        if (mousePosition1.x>mousePosition2.x)
-        {
-            rotate.z = rotate.z + mousePosition1.x - mousePosition2.x;
-
-        }
-    }
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
 
 }
